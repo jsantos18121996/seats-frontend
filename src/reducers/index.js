f2e5100193fbf9294f04858449b2ec915f349fca
jaime <jhorlyn.santos@costamar.com>
2023-08-20 20:39:48 -0500

    Se corrige importacion de archivo SeatsReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux';
-import seatsReducer from './seatsReducer';
+import seatsReducer from './SeatsReducer';
 
 export default combineReducers({
     seats: seatsReducer
